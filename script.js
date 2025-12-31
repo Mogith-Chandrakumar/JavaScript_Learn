fda95ba2198c7fb41ad588a8c58a3f64b19f6b07
Mogith-Chandrakumar <mogith2002@gamil.com>
2025-12-31 15:22:46 +0530

    Arithmetic operations

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ let y = 6;
 let z = x + y;
 console.log(z); // Outputs: 11
 
+//Multiplying values:
+let a = x * y;
+console.log(a); // Outputs: 30
+// Dividing values:
+let b = y / x;
+console.log(b); // Outputs: 1.2 
+// Subtracting values:
+let c = y - x;
+console.log(c); // Outputs: 1
+// Modulus operation:
+let d = y % x;
+console.log(d); // Outputs: 1
+// Incrementing values:
+x++;
+console.log(x); // Outputs: 6
+// Decrementing values:
+y--;
+console.log(y); // Outputs: 5   
+// Combining operations:
+let e = (x + y) * 2;
+console.log(e); // Outputs: 22
