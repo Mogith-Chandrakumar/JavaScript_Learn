75ce3b3ec3bfbfc353458a5acda7d4153075755d
Mogith-Chandrakumar <mogith2002@gamil.com>
2025-12-31 15:25:19 +0530

    Conditional statements

--- script.js
+++ script.js
@@ -27,3 +27,51 @@ console.log(y); // Outputs: 5
 // Combining operations:
 let e = (x + y) * 2;
 console.log(e); // Outputs: 22
+
+// How to Use Conditional Statements:
+if (condition1) {
+  // code to execute if condition1 is true
+} else if (condition2) {
+  // code to execute if the condition1 is false and condition2 is true
+} else {
+  // code to execute if the condition1 is false and condition2 is false
+}
+
+// Example of conditional statements:
+let age = 20;  
+if (age < 18) {
+  console.log("Minor");
+} else if (age >= 18 && age < 65) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+} // Outputs: Adult
+
+// How to Use Loops:
+// For Loop:
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+// Outputs: 0 1 2 3 4   
+
+// While Loop:
+let j = 0;      
+while (j < 5) {
+    console.log(j);     
+    j++;
+}
+// Outputs: 0 1 2 3 4   
+
+// Do-While Loop:
+let k = 0;
+do {
+    console.log(k);
+    k++;
+} while (k < 5);
+// Outputs: 0 1 2 3 4
+
+// How to Define Functions:
+function greet(name) {
+  return "Hello, " + name + "!";
+}   
+console.log(greet("Alice")); // Outputs: Hello, Alice!
